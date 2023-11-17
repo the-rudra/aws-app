396231ef244534019bbd3ce254db0ec0746d93f8
the-rudra <itsrudrapratapdev@gmail.com>
2023-11-17 23:12:10 +0530

    initial commit

---
+++ next.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+    compiler: {
+        styledComponents: {
+            // Enable display of the component name along with the generated className (needed for debugging).
+            displayName: true,
+            // Enable SSR support
+            ssr: true,
+            // Optional
+            fileName: false,
+        },
+    },
+};
